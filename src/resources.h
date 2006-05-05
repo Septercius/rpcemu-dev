1cb0d87a3adf4338d2f81346b88c5401d5e743da
twalker <none@none>
2006-05-05 14:49:09 +0100

    [svn r24] Added missing files
    --HG--
    branch : trunk

---
+++ src/resources.h
@@ -0,0 +1,38 @@
+#define IDM_FILE_RESET    40000
+#define IDM_FILE_EXIT     40001
+#define IDM_DISC_LD0      40010
+#define IDM_DISC_LD1      40011
+#define IDM_MACHINE_CPU   40020
+#define IDM_MACHINE_RAM   40021
+#define IDM_CPU_ARM610    40030
+#define IDM_CPU_ARM710    40031
+#define IDM_CPU_ARM7500   40032
+#define IDM_CPU_SA110     40033
+#define IDM_RAM_4MEGS     40040
+#define IDM_RAM_8MEGS     40041
+#define IDM_RAM_16MEGS    40042
+#define IDM_RAM_32MEGS    40043
+#define IDM_RAM_64MEGS    40044
+#define IDM_RAM_128MEGS   40045
+#define IDM_VRAM_NONE     40050
+#define IDM_VRAM_2MEGS    40051
+#define IDM_CONFIG        40052
+
+#define RadioButton1  1000
+#define RadioButton2  1001
+#define RadioButton3  1002
+#define RadioButton4  1003
+#define RadioButton5  1004
+#define RadioButton6  1005
+#define RadioButton7  1006
+#define RadioButton8  1007
+#define RadioButton9  1008
+#define RadioButton10 1009
+#define RadioButton11 1010
+#define RadioButton12 1011
+
+#define GroupBox1 1020
+#define GroupBox2 1021
+#define GroupBox3 1022
+
+#define CheckBox1 1030
---
+++ src/rpcemu.c
@@ -0,0 +1,106 @@
+#include <allegro.h>
+#include "rpc.h"
+
+char discname[2][260]={"boot.adf","notboot.adf"};
+
+void loadconfig();
+void saveconfig();
+
+int startrpcemu()
+{
+        int c;
+        char *p;
+        get_executable_name(exname,511);
+        p=get_filename(exname);
+        *p=0;
+        append_filename(HOSTFS_ROOT,exname,"hostfs",511);
+        for (c=0;c<511;c++)
+        {
+                if (HOSTFS_ROOT[c]=='\\')
+                   HOSTFS_ROOT[c]='/';
+        }
+        initmem();
+        if (loadroms())
+        {
+                error("RiscOS ROMs missing!");
+                return -1;
+        }
+        resetarm();
+        resetiomd();
+        resetkeyboard();
+        reset82c711();
+        resetide();
+        reseti2c();
+        loadcmos();
+        loadadf("boot.adf",0);
+        loadadf("notboot.adf",1);
+        initvideo();
+        loadconfig();
+        reallocmem(rammask+1);
+        return 0;
+}
+
+void execrpcemu()
+{
+        execarm(80000);
+        if (drawscre)
+        {
+                drawscre=0;
+                iomdvsync();
+                drawscr();
+                pollmouse();
+                pollkeyboard();
+        }
+}
+
+void endrpcemu()
+{
+        saveadf(discname[0], 0);
+        saveadf(discname[1], 1);
+        free(vram);
+        free(ram);
+        free(ram2);
+        free(rom);
+        savecmos();
+        saveconfig();
+}
+
+void loadconfig()
+{
+        char fn[512];
+        char *p;
+        append_filename(fn,exname,"rpc.cfg",511);
+        set_config_file(fn);
+        p=(char *)get_config_string(NULL,"mem_size",NULL);
+        if (!p || !strcmp(p,"4"))  rammask=0x1FFFFF;
+        else if (!strcmp(p,"8"))   rammask=0x3FFFFF;
+        else if (!strcmp(p,"32"))  rammask=0xFFFFFF;
+        else if (!strcmp(p,"64"))  rammask=0x1FFFFFF;
+        else if (!strcmp(p,"128")) rammask=0x3FFFFFF;
+        else                       rammask=0x7FFFFF;
+        p=(char *)get_config_string(NULL,"vram_size",NULL);
+        if (!p || !strcmp(p,"0"))  vrammask=0;
+        else                       vrammask=0x1FFFFF;
+        p=(char *)get_config_string(NULL,"cpu_type",NULL);
+        if (!p || !strcmp(p,"ARM610")) model=1;
+        else if (!strcmp(p,"ARM710"))  model=2;
+        else if (!strcmp(p,"SA110"))   model=3;
+        else                           model=0;
+}
+
+void saveconfig()
+{
+        char s[256];
+        sprintf(s,"%i",((rammask+1)>>20)<<1);
+        set_config_string(NULL,"mem_size",s);
+        switch (model)
+        {
+                case 1: sprintf(s,"ARM610"); break;
+                case 2: sprintf(s,"ARM710"); break;
+                case 3: sprintf(s,"SA110"); break;
+                default: sprintf(s,"ARM7500"); break;
+        }
+        set_config_string(NULL,"cpu_type",s);
+        if (vrammask) set_config_string(NULL,"vram_size","2");
+        else          set_config_string(NULL,"vram_size","0");
+}
