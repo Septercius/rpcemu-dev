3b16c0449eb9c59b704f4a30e0623d6375f7ca3b
twalker <none@none>
2008-05-11 09:37:53 +0100

    [svn r128] Fixed further compilation bug
    --HG--
    branch : trunk

--- src/icside.c
+++ src/icside.c
@@ -9,7 +9,7 @@ int icspage;
 
 uint8_t icsreadb(podule *p, int easi, uint32_t addr)
 {
-        int temp;
+/*        int temp;
 //        rpclog("Read ICSB %04X\n",addr);
         switch (addr&0x3000)
         {
@@ -19,47 +19,47 @@ uint8_t icsreadb(podule *p, int easi, uint32_t addr)
                 case 0x3000:
                 ideboard=2;
                 return readide(((addr>>2)&7)+0x1F0);
-        }
+        }*/
 }
 
 uint16_t icsreadw(podule *p, int easi, uint32_t addr)
 {
-        if ((addr&0x3000)==0x3000)
+/*        if ((addr&0x3000)==0x3000)
         {
 //                rpclog("Read IDEW\n");
                 ideboard=2;
                 return readidew();
         }
-        return icsreadb(p,easi,addr);
+        return icsreadb(p,easi,addr);*/
 }
 
 void icswriteb(podule *p, int easi, uint32_t addr, uint8_t val)
 {
 //        rpclog("Write ICSB %04X %02X\n",addr,val);
-        switch (addr&0x3000)
+/*        switch (addr&0x3000)
         {
                 case 0x2000: icspage=val; return;
                 case 0x3000:
                 ideboard=2;
                 writeide(((addr>>2)&7)+0x1F0,val);
                 return;
-        }
+        }*/
 }
 
 void icswritew(podule *p, int easi, uint32_t addr, uint16_t val)
 {
-        if ((addr&0x3000)==0x3000)
+/*        if ((addr&0x3000)==0x3000)
         {
 //                rpclog("WRITEIDEW\n");
                 ideboard=2;
                 return writeidew(val);
         }
-        icswriteb(p,easi,addr,val);
+        icswriteb(p,easi,addr,val);*/
 }
 
 void initics()
 {
-        FILE *f;
+/*        FILE *f;
         char fn[512];
         append_filename(fn,exname,"zidefs",sizeof(fn));
         f=fopen(fn,"rb");
@@ -70,6 +70,6 @@ void initics()
         }
         fread(icsrom,8192,1,f);
         fclose(f);
-        addpodule(NULL,icswritew,icswriteb,NULL,icsreadw,icsreadb,NULL,NULL);
+        addpodule(NULL,icswritew,icswriteb,NULL,icsreadw,icsreadb,NULL,NULL);*/
 //        rpclog("ICS Initialised!\n");
 }
