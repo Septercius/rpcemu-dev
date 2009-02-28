842dcf646b54baa3e6f275267da92d3eac84867b
matthew <none@none>
2009-02-28 18:21:21 +0000

    Simplify use of IO port with read82c711() and write82c711()
    --HG--
    branch : trunk

--- src/82c711.c
+++ src/82c711.c
@@ -302,11 +302,13 @@ static void writefdc(uint32_t addr, uint32_t val)
 void write82c711(uint32_t addr, uint32_t val)
 {
 	static unsigned char printstat = 0;
-        uint32_t addr2;
-        addr2=(addr>>2)&0x3FF;
+
+        /* Convert memory-mapped address to IO port */
+        addr = (addr >> 2) & 0x3ff;
+
         if (configmode!=2)
         {
-                if ((addr2==0x3F0) && (val==0x55))
+                if ((addr == 0x3F0) && (val==0x55))
                 {
                         configmode++;
                         return;
@@ -314,34 +316,34 @@ void write82c711(uint32_t addr, uint32_t val)
                 else
                    configmode=0;
         }
-        if (configmode==2 && addr2==0x3F0 && val==0xAA)
+        if (configmode==2 && addr == 0x3F0 && val==0xAA)
         {
                 configmode=0;
 //                printf("Cleared config mode\n");
                 return;
         }
-//        if (addr2>=0x278 && addr2<=0x27A)
-//           rpclog("Write 82c711 %08X %08X %03X %07X %08X\n",addr,val,addr2,PC,armregs[12]);
-        if ((addr2>=0x3F0) && (addr2<=0x3F7)) writefdc(addr2, val);
-        if ((addr2==0x27A) && ((val&0x10) || ((printstat^val)&1 && !(val&1))))
+//        if (addr >= 0x278 && addr <= 0x27A)
+//           rpclog("Write 82c711 %03X %08X %07X %08X\n",addr,val,PC,armregs[12]);
+        if ((addr >= 0x3F0) && (addr <= 0x3F7)) writefdc(addr, val);
+        if ((addr == 0x27A) && ((val&0x10) || ((printstat^val)&1 && !(val&1))))
         {
 //                rpclog("Printer interrupt %02X\n",iomd.maska);
                 iomd.stata|=1;
                 updateirqs();
         }
-        if (addr2==0x27A) printstat=val;
-        if ((addr2==0x3F9) && (val&2))
+        if (addr == 0x27A) printstat=val;
+        if ((addr == 0x3F9) && (val&2))
         {
 //                printf("Serial transmit empty interrupt\n");
                 iomd.statf|=0x10;
                 updateirqs();
         }
-        if (addr2==0x3FB) linectrl=val;
-        if (addr2==0x3FE) scratch=val;
-        if ((addr2>=0x1F0 && addr2<=0x1F7) || addr2==0x3F6)
+        if (addr == 0x3FB) linectrl=val;
+        if (addr == 0x3FE) scratch=val;
+        if ((addr >= 0x1F0 && addr <= 0x1F7) || addr == 0x3F6)
         {
                 ideboard=0;
-                writeide(addr2,val);
+                writeide(addr, val);
                 return;
         }
 }
@@ -403,25 +405,26 @@ static void fdcsenddata(uint8_t val)
 
 uint8_t read82c711(uint32_t addr)
 {
-        uint32_t addr2;
-        addr2=(addr>>2)&0x3FF;
-        if (addr2==0x279) return 0x90;
-        if ((addr2>=0x1F0 && addr2<=0x1F7) || addr2==0x3F6)
+        /* Convert memory-mapped address to IO port */
+        addr = (addr >> 2) & 0x3ff;
+
+        if (addr == 0x279) return 0x90;
+        if ((addr >= 0x1F0 && addr <= 0x1F7) || addr == 0x3F6)
         {
                 ideboard=0;
-                return readide(addr2);
+                return readide(addr);
         }
-        if ((addr2>=0x3F0) && (addr2<=0x3F7)) return readfdc(addr2);
-        if (addr2==0x3FA)
+        if ((addr >= 0x3F0) && (addr <= 0x3F7)) return readfdc(addr);
+        if (addr == 0x3FA)
         {
                 iomd.statf&=~0x10;
                 updateirqs();
                 return 2;
         }
-        if (addr2==0x3FB) return linectrl;
-        if (addr2==0x3FE) return scratch;
+        if (addr == 0x3FB) return linectrl;
+        if (addr == 0x3FE) return scratch;
 
-/*        if (addr2==0x3F6)
+/*        if (addr == 0x3F6)
         {
                 ide.atastat+=0x40;
                 return ide.atastat&0xC0;
