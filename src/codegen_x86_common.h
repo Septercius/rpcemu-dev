0fe13cdebfc4c170089f24dcccb0ecfe25210bd3
matthew <none@none>
2009-09-13 15:39:06 +0100

    Move x86 register #defines into a new common file
    --HG--
    branch : trunk

---
+++ src/codegen_x86_common.h
@@ -0,0 +1,15 @@
+#ifndef CODEGEN_X86_COMMON_H
+#define CODEGEN_X86_COMMON_H
+
+/* x86 registers (bitfields for instruction encoding) */
+#define EAX	0x00
+#define ECX	0x08
+#define EDX	0x10
+#define EBX	0x18
+#define ESP	0x20
+#define EBP	0x28
+#define ESI	0x30
+#define EDI	0x38
+
+#endif /* CODEGEN_X86_COMMON_H */
+
