0fed1a0a7707a86e16bbcc1b4d7854dd0960c7ca
peterh <none@none>
2011-09-03 16:40:40 +0100

    Allow CallASWI for intercepted SWIs. Based on a patch by Alan Buckley
    --HG--
    branch : trunk

--- src/arm_common.c
+++ src/arm_common.c
@@ -20,6 +20,12 @@
 #include "network.h"
 #endif
 
+#define SWI_OS_Byte		0x6
+#define SWI_OS_Word		0x7
+#define SWI_OS_Mouse		0x1c
+#define SWI_OS_CallASWI		0x6f
+#define SWI_OS_CallASWIR12	0x71
+
 /**
  * Handler for SWI instructions; includes all the emulator specific SWIs as
  * well as the standard SWI interface of raising an exception.
@@ -31,13 +37,19 @@
 void
 opSWI(uint32_t opcode)
 {
-	uint32_t templ;
+	uint32_t swinum = opcode & 0xdffff;
 
 	inscount++;
 	rinscount++;
-	templ = opcode & 0xdffff;
 
-	if (mousehack && templ == 7 && armregs[0] == 0x15) {
+	/* Get actual SWI number from OS_CallASWI and OS_CallASWIR12 */
+	if (swinum == SWI_OS_CallASWI) {
+		swinum = armregs[10] & 0xdffff;
+	} else if (swinum == SWI_OS_CallASWIR12) {
+		swinum = armregs[12] & 0xdffff;
+	}
+
+	if (mousehack && swinum == SWI_OS_Word && armregs[0] == 21) {
 		if (readmemb(armregs[1]) == 1) {
 			/* OS_Word 21, 1 Define Mouse Coordinate bounding box */
 			mouse_hack_osword_21_1(armregs[1]);
@@ -50,18 +62,18 @@ opSWI(uint32_t opcode)
 			goto realswi;
 		}
 
-	} else if (mousehack && templ == 0x1c) {
+	} else if (mousehack && swinum == SWI_OS_Mouse) {
 		/* OS_Mouse */
 		mouse_hack_osmouse();
 		armregs[cpsr] &= ~VFLAG;
 
-	} else if (templ == ARCEM_SWI_HOSTFS) {
+	} else if (swinum == ARCEM_SWI_HOSTFS) {
 		ARMul_State state;
 
 		state.Reg = armregs;
 		hostfs(&state);
 
-	} else if (templ == ARCEM_SWI_NANOSLEEP) {
+	} else if (swinum == ARCEM_SWI_NANOSLEEP) {
 #ifdef RPCEMU_WIN
 		Sleep(armregs[0] / 1000000);
 #else
@@ -74,7 +86,7 @@ opSWI(uint32_t opcode)
 		armregs[cpsr] &= ~VFLAG;
 	}
 #ifdef RPCEMU_NETWORKING
-	else if (templ == ARCEM_SWI_NETWORK) {
+	else if (swinum == ARCEM_SWI_NETWORK) {
 		if (config.network_type != NetworkType_Off) {
 			networkswi(armregs[0], armregs[1], armregs[2], armregs[3],
 			           armregs[4], armregs[5], &armregs[0], &armregs[1]);
@@ -83,13 +95,13 @@ opSWI(uint32_t opcode)
 #endif
 	else {
 realswi:
-		if (mousehack && templ == 7 && armregs[0] == 0x15 &&
+		if (mousehack && swinum == SWI_OS_Word && armregs[0] == 21 &&
 		    readmemb(armregs[1]) == 0)
 		{
 			/* OS_Word 21, 0 Define pointer size, shape and active point */
 			mouse_hack_osword_21_0(armregs[1]);
 		}
-		if (mousehack && templ == 6 && armregs[0] == 106) {
+		if (mousehack && swinum == SWI_OS_Byte && armregs[0] == 106) {
 			/* OS_Byte 106 Select pointer / activate mouse */
 			mouse_hack_osbyte_106(armregs[1]);
 		}
