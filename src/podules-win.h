708871fe7d333a0dfbf70c32c3b97faa35b42353
matthew <none@none>
2013-03-28 22:37:45 +0000

    Remove redundant definition of struct podule from podules-win.h
    --HG--
    branch : trunk

--- src/podules-win.h
+++ src/podules-win.h
@@ -5,21 +5,6 @@
 extern "C" {
 #endif
 
-typedef struct
-{
-        void (*writeb)(void *p, int easi, uint32_t addr, uint8_t val);
-        void (*writew)(void *p, int easi, uint32_t addr, uint16_t val);
-        void (*writel)(void *p, int easi, uint32_t addr, uint32_t val);
-        uint8_t  (*readb)(void *p, int easi, uint32_t addr);
-        uint16_t (*readw)(void *p, int easi, uint32_t addr);
-        uint32_t (*readl)(void *p, int easi, uint32_t addr);
-        int (*timercallback)(void *p);
-        void (*reset)(void);
-        int irq,fiq;
-        int msectimer;
-} podule;
-              
-
 extern void opendlls(void);
 
 #ifdef __cplusplus
