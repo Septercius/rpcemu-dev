4b38f5edc19525e344dcee5048ac9a8579b24539
matthew <none@none>
2015-11-03 16:20:16 +0000

    No longer need 4 extra blocks in rcodeblock[] - these previously contained support code for LDM/STM
    --HG--
    branch : trunk

--- src/codegen_x86.h
+++ src/codegen_x86.h
@@ -4,7 +4,7 @@
 #define BLOCKS 1024
 
 //unsigned char codeblock[3][0x1000][1600];
-extern unsigned char rcodeblock[BLOCKS+4][1792+512+64];
+extern unsigned char rcodeblock[BLOCKS][1792+512+64];
 extern unsigned long codeblockpc[0x8000];
 extern int codeblocknum[0x8000];
 
