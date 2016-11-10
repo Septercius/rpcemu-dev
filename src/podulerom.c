627bd252edf96480780db6e20a35d3e1a08b7097
peterh <none@none>
2016-11-10 17:30:22 +0000

    Reformat code in podulerom.c
    --HG--
    branch : trunk

--- src/podulerom.c
+++ src/podulerom.c
@@ -13,7 +13,7 @@
 #include "podulerom.h"
 
 #define MAXROMS 16
-static char romfns[MAXROMS+1][256];
+static char romfns[MAXROMS + 1][256];
 
 static uint8_t *podulerom = NULL;
 static uint32_t poduleromsize = 0;
@@ -56,13 +56,14 @@ readpodulerom(podule *p, int easi, uint32_t addr)
 {
 	NOT_USED(p);
 
-        if (easi && (poduleromsize>0))
-        {
-                addr=(addr&0x00FFFFFF)>>2;
-                if (addr<poduleromsize) return podulerom[addr];
-                return 0x00;
-        }
-        return 0xFF;
+	if (easi && (poduleromsize > 0)) {
+		addr = (addr & 0x00ffffff) >> 2;
+		if (addr < poduleromsize) {
+			return podulerom[addr];
+		}
+		return 0x00;
+	}
+	return 0xff;
 }
 
 /**
@@ -85,8 +86,8 @@ podulerom_reset(void)
 void
 initpodulerom(void)
 {
-        int file=0;
-        int i;
+	int file = 0;
+	int i;
 	char romdirectory[512];
 	DIR *dir;
 	const struct dirent *d;
@@ -94,8 +95,10 @@ initpodulerom(void)
 	/* Build podulerom directory path */
 	snprintf(romdirectory, sizeof(romdirectory), "%spoduleroms/", rpcemu_get_datadir());
 
-        if (podulerom) free(podulerom);
-        poduleromsize = 0;
+	if (podulerom != NULL) {
+		free(podulerom);
+	}
+	poduleromsize = 0;
 
 	/* Scan directory for podule files */
 	dir = opendir(romdirectory);
@@ -121,55 +124,62 @@ initpodulerom(void)
 	}
 
 	/* Build podulerom header */
-        chunkbase = 0x10;
-        filebase = chunkbase + 8 * file + 8;
-        poduleromsize = filebase + ((sizeof(description)+3) &~3); /* Word align description string */
-        podulerom = malloc(poduleromsize);
-        if (podulerom == NULL) fatal("Out of Memory");
+	chunkbase = 0x10;
+	filebase = chunkbase + 8 * file + 8;
+	poduleromsize = filebase + ((sizeof(description) + 3) & ~3u); /* Word align description string */
+	podulerom = malloc(poduleromsize);
+	if (podulerom == NULL) {
+		fatal("Out of Memory");
+	}
 
-        memset(podulerom, 0, poduleromsize);
-        podulerom[0] = 0; /* Acorn comformant card, not requesting FIQ, not requesting interupt, EcID = 0 = EcID is extended (8 bytes) */
-        podulerom[1] = 3; /* Interrupt status has been relocated, chunk directories present, byte access */
-        podulerom[2] = 0; /* Mandatory */
-        podulerom[3] = 0; /* Product type, low,  ???? */
-        podulerom[4] = 0; /* Product type, high, ???? */
-        podulerom[5] = 0; /* Manufacturer, low,  Acorn UK */
-        podulerom[6] = 0; /* Manufacturer, high, Acorn UK */
-        podulerom[7] = 0; /* Reserved */
+	memset(podulerom, 0, poduleromsize);
+	podulerom[0] = 0; /* Acorn comformant card, not requesting FIQ, not requesting interupt, EcID = 0 = EcID is extended (8 bytes) */
+	podulerom[1] = 3; /* Interrupt status has been relocated, chunk directories present, byte access */
+	podulerom[2] = 0; /* Mandatory */
+	podulerom[3] = 0; /* Product type, low,  ???? */
+	podulerom[4] = 0; /* Product type, high, ???? */
+	podulerom[5] = 0; /* Manufacturer, low,  Acorn UK */
+	podulerom[6] = 0; /* Manufacturer, high, Acorn UK */
+	podulerom[7] = 0; /* Reserved */
 
-        memcpy(podulerom + filebase, description, sizeof(description));
-        makechunk(0xF5, filebase, sizeof(description)); /* F = Device Data, 5 = description */
-        filebase+=(sizeof(description)+3)&~3;
+	memcpy(podulerom + filebase, description, sizeof(description));
+	makechunk(0xf5, filebase, sizeof(description)); /* f = Device Data, 5 = description */
+	filebase += (sizeof(description) + 3) & ~3u;
 
 	/* Add each file into the podule's rom */
-        for (i=0;i<file;i++)
-        {
-                FILE *f;
-                char filepath[512];
-                int len;
+	for (i = 0; i < file; i++) {
+		FILE *f;
+		char filepath[512];
+		int len;
 
-                snprintf(filepath, sizeof(filepath), "%s%s", romdirectory, romfns[i]);
+		snprintf(filepath, sizeof(filepath), "%s%s", romdirectory, romfns[i]);
 
-                f = fopen(filepath, "rb");
-                if (f==NULL) fatal("Can't open podulerom file\n");
-                fseek(f,-1,SEEK_END);
-                len = ftell(f) + 1;
-                poduleromsize += (len+3)&~3;
-                if (poduleromsize > 4096*1024) fatal("Cannot have more than 4MB of podule ROM");
-                podulerom = realloc(podulerom, poduleromsize);
-                if (podulerom == NULL) fatal("Out of Memory");
+		f = fopen(filepath, "rb");
+		if (f == NULL) {
+			fatal("Can't open podulerom file\n");
+		}
+		fseek(f, -1, SEEK_END);
+		len = ftell(f) + 1;
+		poduleromsize += (len + 3) & ~3u;
+		if (poduleromsize > 4096 * 1024) {
+			fatal("Cannot have more than 4MB of podule ROM");
+		}
+		podulerom = realloc(podulerom, poduleromsize);
+		if (podulerom == NULL) {
+			fatal("Out of Memory");
+		}
 
-                fseek(f,0,SEEK_SET);
+		fseek(f, 0, SEEK_SET);
 		if (fread(podulerom + filebase, 1, len, f) != len) {
 			fatal("initpodulerom: Failed to read file '%s': %s",
 			      romfns[i], strerror(errno));
 		}
-                fclose(f);
+		fclose(f);
 		rpclog("initpodulerom: Successfully loaded '%s' into podulerom\n",
 		       romfns[i]);
-                makechunk(0x81, filebase, len); /* 8 = Mandatory, Acorn Operating System #0 (RISC OS), 1 = BBC ROM */
-                filebase+=(len+3)&~3;
-        }
+		makechunk(0x81, filebase, len); /* 8 = Mandatory, Acorn Operating System #0 (RISC OS), 1 = BBC ROM */
+		filebase += (len + 3) & ~3u;
+	}
 
-        addpodule(NULL,NULL,NULL,NULL,NULL,readpodulerom,NULL,NULL,0);
+	addpodule(NULL, NULL, NULL, NULL, NULL, readpodulerom, NULL, NULL, 0);
 }
