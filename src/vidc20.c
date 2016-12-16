34e4fd774091112f7e28f39a87bbc6a97ab3d115
peterh <none@none>
2016-12-16 14:15:37 +0000

    Add GPL header to source files
    --HG--
    branch : trunk

--- src/vidc20.c
+++ src/vidc20.c
@@ -1,6 +1,25 @@
-/*RPCemu v0.6 by Tom Walker
-  VIDC20 emulation*/
 /*
+  RPCEmu - An Acorn system emulator
+
+  Copyright (C) 2005-2010 Sarah Walker
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* VIDC20 emulation
+
  References:
    ARM VIDC20 Datasheet - ARM DDI 0030E
    Acorn Risc PC Technical Reference Manual - ISBN 1 85250 144 8
