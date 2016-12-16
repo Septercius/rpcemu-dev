6272a416db8537c5d9c4e2e48beb7a11c2f08b1f
peterh <none@none>
2016-12-16 14:15:37 +0000

    Add GPL header to source files
    --HG--
    branch : trunk

--- src/rpcemu.c
+++ src/rpcemu.c
@@ -1,6 +1,25 @@
-/*RPCemu v0.6 by Tom Walker
-  Main loop
-  Should be platform independent*/
+/*
+  RPCEmu - An Acorn system emulator
+
+  Copyright (C) 2005-2010 Sarah Walker
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* Main loop
+   Should be platform independent */
 #include <assert.h>
 #include <stdarg.h>
 #include <stdint.h>
--- src/win/rpc-win.c
+++ src/win/rpc-win.c
@@ -1,8 +1,24 @@
-/*RPCemu v0.6 by Tom Walker
-  Windows specific stuff
-  Also includes most sound emulation, since that is very Windows specific at
-  the moment.
-  Since I'm the only maintainer of this file, it's a complete mess.*/
+/*
+  RPCEmu - An Acorn system emulator
+
+  Copyright (C) 2005-2010 Sarah Walker
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* Windows specific stuff */
 
 #include <assert.h>
 #include <stdio.h>
